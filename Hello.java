47c75573450ba406c1e139da5ee04f6d01932093
Asif-05 <104357431+Asif-05@users.noreply.github.com>
2023-11-02 19:51:37 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Hello {
+	
+	public static void welcome(int x,int y)
+	{
+		int z=x+y;
+	    System.out.println(z);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		 welcome(5,4);
+		
+		
+	}
+
+}
