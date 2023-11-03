8179e991913e8fa391db4a706da5e33578686367
Asif-05 <104357431+Asif-05@users.noreply.github.com>
2023-11-03 13:23:13 +0530

    Add files via upload

---
+++ HelloHow.java
@@ -0,0 +1,19 @@
+package mycode;
+
+public class HelloHow {
+	
+	public static void welcome(int x,int y)
+	{
+		int z=x+y;
+	    System.out.println(z);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		 welcome(5,4);
+		
+		
+	}
+
+}
